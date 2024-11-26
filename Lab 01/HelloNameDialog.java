cf47033bd37cd68eb75aba3ce06306d48868a83c
Kaivu10102004 <164660261+Kaivu10102004@users.noreply.github.com>
2024-11-26 11:33:27 +0700

    add lab3

--- Lab 01/HelloNameDialog.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-public class HelloNameDialog {
-	public static void main(String[] args) {
-		String result;
-		result = JOptionPane.showInputDialog("Please Enter Your Name:");
-		JOptionPane.showMessageDialog(null, "Hi " + result + " !");
-		System.exit(0);
-	}
-}
